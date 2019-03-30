64443af2de93175685964d8dd8c2b7e7464fb737
Loïc <lolo.mess.17@gmail.com>
2019-03-30 11:35:35 +0100

    Base upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+const pug = require('pug');
+const compiledFunction = pug.compileFile('template.pug');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/assets", express.static(path.join(__dirname, 'assets')));
+app.get('/accueil', function (req, res) {
+    res.send(pug.renderFile('template.pug', {
+        activePage: 'accueil'
+    }));
+})
+
+app.post('/search/', (req, res) => {
+    res.send(pug.renderFile('template.pug', {
+        activePage: 'search',
+        searchDept: req.body.search.dept,
+        searchTypeBien: req.body.search.typeBien,
+        searchTypeAchat: req.body.search.typeAchat,
+        searchVille: req.body.search.ville
+    }));
+})
+
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!')
+})
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: process.env.MYSQL_DATABASE_CONNECTION
+});
+var bookshelf = require('bookshelf')(knex);
+
+var User = bookshelf.Model.extend({
+tableName: 'users',
+posts: function() {
+    return this.hasMany(Posts);
+}
+});
+
+var Posts = bookshelf.Model.extend({
+    tableName: 'messages',
+    tags: function() {
+        return this.belongsToMany(Tag);
+    }
+});
+
+var Tag = bookshelf.Model.extend({
+    tableName: 'tags'
+})
+
+User.where('id', 1).fetch({withRelated: ['posts.tags']}).then(function(user) {
+    console.log(user.related('posts').toJSON());
+}).catch(function(err) {
+    console.error(err);
+});
